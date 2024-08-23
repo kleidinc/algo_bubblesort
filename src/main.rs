c7b8359f5d473ef9b1783f717780b1d60e3dd949
Alex K <kleydints@petalmail.com>
2024-08-23 11:21:15 +0300

    added helpers for generating random vectors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+fn bubblesort(collection: &mut Vec<i32>) -> &Vec<i32> {
+    todo!();
 }
+fn main() {
+    // 1. reate vector with random numbers
+    // 2. sort the vector using bubblesort
+    // 3. print the sorted version to the terminal
+}
+
+mod helpers;
